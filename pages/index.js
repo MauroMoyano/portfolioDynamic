793d39f5eab97d1a50be89d09534066a208556e3
MauroMoyano <mauromoyanodev@gmail.com>
2023-08-01 19:16:09 -0300

    working on home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+// next image
+import Image from 'next/image'
+
+// components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// framer motion
+import {motion} from 'framer-motion'
+
+//variants
+import {fadeIn} from '../variants'
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div className='bg-primary/60'>Home</div>;
 };
 
 export default Home;
