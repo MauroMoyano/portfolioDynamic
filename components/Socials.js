3248f081030b75631f69bf18b0a2583d77959c23
Mauro Moyano <mauromoyanodev@gmail.com>
2023-12-20 12:58:00 -0300

    About started

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,37 @@
+// links
+import Link from "next/link";
+
+//icons
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiGithubLine,
+  RiPinterestFill,
+  RiHeading
+} from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiLinkedinLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiGithubLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiInstagramLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiHeading />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiYoutubeLine />
+    </Link>
+    <Link href={''} className="hover:text-accent transition-all duration-300" >
+      <RiPinterestFill />
+    </Link>
+  </div>;
 };
 
 export default Socials;
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,13 +1,29 @@
-//  data
-import {FaCss3, FaFigma, FaHtml5, FaJs, FaReact, FaWordpress} from "react-icons/fa";
-import {SiAdobephotoshop, SiAdobexd, SiFramer, SiNextdotjs} from "react-icons/si";
+import React, { useState } from "react";
 
-const aboutData = [
+//  icons
+import {
+  FaCss3,
+  FaFigma,
+  FaHtml5,
+  FaJs,
+  FaReact,
+  FaWordpress,
+} from "react-icons/fa";
+
+import {
+  SiAdobephotoshop,
+  SiAdobexd,
+  SiFramer,
+  SiNextdotjs,
+} from "react-icons/si";
+
+// about data
+export const aboutData = [
   {
-    title: 'skills',
+    title: "skills",
     info: [
       {
-        title: 'Web Development',
+        title: "Web Development",
         icons: [
           <FaHtml5 />,
           <FaCss3 />,
@@ -19,62 +35,70 @@ const aboutData = [
         ],
       },
       {
-        title: 'UI/UX Design',
+        title: "UI/UX Design",
         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
       },
     ],
   },
   {
-    title: 'awards',
+    title: "awards",
     info: [
       {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: "Webby Awards - Honoree",
+        stage: "2011 - 2012",
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
+        title: "Adobe Design Achievement Awards - Finalist",
+        stage: "2009 - 2010",
       },
     ],
   },
   {
-    title: 'experience',
+    title: "experience",
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: "UX/UI Designer - XYZ Company",
+        stage: "2012 - 2023",
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: "Web Developer - ABC Agency",
+        stage: "2010 - 2012",
       },
       {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        title: "Intern - DEF Corporation",
+        stage: "2008 - 2010",
       },
     ],
   },
   {
-    title: 'credentials',
+    title: "credentials",
     info: [
       {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
+        title: "Web Development - ABC University, LA, CA",
+        stage: "2011",
       },
       {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
+        title: "Computer Science Diploma - AV Technical Institute",
+        stage: "2009",
       },
       {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
+        title: "Certified Graphic Designer - ABC Institute, Los Angeles, CA",
+        stage: "2006",
       },
     ],
   },
 ];
 
+// components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0)
+  return <div>
+    <Circles />
+  </div>;
 };
 
 export default About;
