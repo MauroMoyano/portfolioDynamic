1903f50d6322c45f8443d878adbeeb8beeac6801
Mauro Moyano <mauromoyanodev@gmail.com>
2023-12-22 03:54:43 -0300

    Services done

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React, { useState } from "react";
 
 //  icons
@@ -120,17 +121,35 @@ const About = () => {
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* text */}
         <div className="flex-1 flex flex-col justify-center">
-          <h2 className="h2">
+          <motion.h2
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
             Captivating <span className="text-accent">stories</span> birth
             magnificent designs
-          </h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
-            10 years ago, I began freelancing as a developer. Since then , I've
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            10 years ago, I began freelancing as a developer. Since then , I`ve
             done remote work for agencies, consulted for startups, and
             collaborated on digital products for business and consumer use.
-          </p>
+          </motion.p>
           {/* counters */}
-          <div>
+          <motion.div
+            variants={fadeIn("left", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
             <div className="flex flex-1 xl:gap-x-6">
               {/* experience */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
@@ -150,7 +169,7 @@ const About = () => {
                   Satisfied clients
                 </div>
               </div>
-                {/* Finished projects */}
+              {/* Finished projects */}
               <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent">
                   <Counter start={0} end={15} duration={5} delay={1} />+
@@ -159,8 +178,8 @@ const About = () => {
                   Finished projects
                 </div>
               </div>
-                {/* awards */}
-                <div className="relative flex-1 ">
+              {/* awards */}
+              <div className="relative flex-1 ">
                 <div className="text-2xl xl:text-4xl font-extrabold text-accent">
                   <Counter start={0} end={8} duration={5} delay={1} />+
                 </div>
@@ -169,10 +188,16 @@ const About = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         </div>
         {/* info */}
-        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px]"
+        >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
@@ -214,7 +239,7 @@ const About = () => {
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
